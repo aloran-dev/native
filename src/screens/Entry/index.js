621f9887747a2cc8dd992ef92e8cf8fd2764bdf2
Edgardo Morales <emoron@gmail.com>
2019-10-14 15:56:44 -0500

    Fix Entry

--- src/screens/Entry/index.js
+++ src/screens/Entry/index.js
@@ -29,7 +29,9 @@ export default class Entry extends Component{
   constructor(props){
     super(props);
     this.state={
+      scanner:undefined,
       email_contratista:null,
+      token:'',
       email_empleado_seguridad:null,
       planta_area: "Taller",
       invalidCode:false,
@@ -47,83 +49,86 @@ export default class Entry extends Component{
   async componentDidMount(){
     console.log("Entre a Entry",this.props.navigation)
 
+    const codeQr = this.props.navigation.getParam("contratistaQR", "No data read");
+    const scanner = this.props.navigation.getParam("scanner", () => false);
+
+    // var codeQr = this.props.navigation.state.params.contratistaQR;
+    if (codeQr === 'No data read') {
+      this.setState({ invalidCode: true, scanner: scanner });
+
+     }
     //var TokenJWT = await server.login();
     var TokenJWT = await AsyncStorage.getItem('AUTH_TOKEN');
     const api_url = await AsyncStorage.getItem('API_URL')
     const email_seguridad = await AsyncStorage.getItem('ACCOUNT_ID')
 
-    this.setState(
-      {
-        token:TokenJWT,
-        email_empleado_seguridad:this.props.navigation.state.params.email_empleado_seguridad
-      })
+    //  this.setState({token:TokenJWT,email_empleado_seguridad:email_empleado_seguridad})
 
-    console.log("Descargardo Datos",TokenJWT,"API",api_url)
-    console.log(this.props.navigation.state.params.contratistaQR);
-
-    var codeQr = this.props.navigation.state.params.contratistaQR;
-     if(codeQr !== ''){
-        // console.log("Descargardo Datos",TokenJWT,codeQr)
-         const value = codeQr.replace(/\s/g,'');
-
-         //const infoContratista = await
-         server.getContratistaInfoByNanoId(TokenJWT,codeQr)
-         .then((response=>{
+    //const value = codeQr.replace(/\s/g,'');
+   //const infoContratista = await
+   server.getContratistaInfoByNanoId(TokenJWT,codeQr)
+         .then(response=>{
            console.log("Validando QR",response);
-            const image_url =`${api_url}/api/v0/uploads/${response.image_profile}`
-            console.log(image_url);
-
-            this.setState(
-               {
-                 email_contratista:response.email,
-                 image_contractor:{uri:image_url}
-               })
-
-         }))
+           if(response.data){
+             try {
+                  const image_url =`${api_url}/api/v0/uploads/${response.data.image_profile}`
+                  this.setState(
+                       {
+                        token:TokenJWT,
+                        email_empleado_seguridad:email_seguridad,
+                        email_contratista:response.data.email,
+                        image_contractor:{uri:image_url},
+                        scanner: scanner
+                       })
+             }
+             catch (error) {
+                 throw new Error(error);
+             }
+           }else{
+             throw new Error(response.message);
+           }
+         })
          .catch((error)=>{
            console.log("Error en el QR",error)
            this.setState(
              {
-             token:TokenJWT,
-             //email_contratista:infoContratista[0].email,
-             //email_empleado_seguridad:this.props.navigation.state.params.email_empleado_seguridad
+             scanner: scanner,
              invalidCode:true
            })
          })
-
-
-    }
 }
 
   handleCheckIn = async () =>{
       this.setState({isLoading:true})
+
       const newEntry = {
         email_contratista:this.state.email_contratista,
         email_empleado_seguridad:this.state.email_empleado_seguridad,
-    //    planta: this.state.planta,
         planta_area: this.state.planta_area
       };
       console.log("DAtos Entry",JSON.stringify(newEntry));
-
-
     server.add_checkIn(this.state.token,newEntry)
       .then((response)=>{
-        let message = JSON.stringify(response)
-        try{
-           message = `OperationID:${response.transactionId}`
-        }
-        catch(error){
-          console.log(error)
-        }
-        Alert.alert(
-          'CheckIn Confirmation',
-          message,
-          [
-          //  {text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
-            {text: 'Yes', onPress: ()=>this.props.navigation.navigate('Home')},
-          ],
-          { cancelable: false }
-        )
+          let message = JSON.stringify(response)
+          console.log(message)
+          try{
+             message = `OperationID:${response.transactionId}`
+          }
+          catch(error){
+            console.log(error)
+          }
+          Alert.alert(
+            'CheckIn Confirmation',
+            message,
+            [
+            //  {text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
+              {text: 'Yes', onPress: ()=>{
+                this.state.scanner.reactivate();
+                return this.props.navigation.navigate('Companies');
+              }},
+            ],
+            { cancelable: false }
+          )
         //this.props.navigation.navigate('App');
         //this.navigationWithPush('ContractorList');
       })
@@ -161,7 +166,13 @@ export default class Entry extends Component{
             message,
             [
               //{text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
-              {text: 'Ok', onPress: ()=>this.props.navigation.navigate('Home')},
+              {
+                text: 'Ok',
+                onPress:()=>{
+                this.state.scanner.reactivate();
+                return this.props.navigation.navigate('Companies');
+                }
+              },
             ],
             { cancelable: false }
           )
@@ -176,39 +187,41 @@ export default class Entry extends Component{
 
       }
 
-
+  scanQRCodeAgain(){
+    this.state.scanner.reactivate();
+    this.props.navigation.navigate('Companies');
+  }
 
   render(){
 
     if(this.state.invalidCode){
       return (
-          <Container style={styles.main}>
-          <Header style={styles.header}>
-            <Left>
-              <Button
-                transparent
-                onPress={() => this.props.navigation.openDrawer()}>
-                <Icon style={styles.header__text} name="menu" />
-              </Button>
-            </Left>
-            <Body>
-              <Title style={styles.header__text}>CertiFast</Title>
-            </Body>
-          </Header>
-            <Content button style={styles.maincontent}>
-              <Text note>Invalid QR Code</Text>
-              <Button
-                rounded
-                block
-                style={styles.button}
-                onPress={()=>{this.props.navigation.navigate('Contractors')}}
-              >
-              <Text>Back</Text>
-              </Button>
-            </Content>
-          </Container>
+        <Container style={styles.main}>
+          <Header title="CertiFast" />
+          <Content style={styles.maincontent}>
+            <ContractorHeader
+              nombre=""
+              apellido=""
+              imgUrl=""
+            />
+            <View style={styles.cardscontainer}>
+              <Card style={styles.card}>
 
+                <Item style={styles.card__item}>
+                    <Text>QR Code Invalid</Text>
+                </Item>
+
+                <View style={styles.footerButtons}>
+                  <Button onPress={this.scanQRCodeAgain()} transparent>
+                    <Text style={{color: '#ff2d2d'}}>Scan another code</Text>
+                  </Button>
+                </View>
+              </Card>
+            </View>
+          </Content>
+        </Container>
       )
+
     }else{
       return (
         <Container style={styles.main}>
